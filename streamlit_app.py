d12721c36cc807dd6cb76986947caf42e30c33ba
champrats <122062956+champrats@users.noreply.github.com>
2025-03-19 13:57:32 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,10 +44,10 @@ if ingredients_list:
         fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    # my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            # values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
-    # st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     # st.stop()
     
     time_to_insert = st.button('Submit Order')
